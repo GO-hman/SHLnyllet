1dddc98364b68a17fef4e14ee7efd113c40d5e90
GO-hman <glateohman@gmail.com>
2026-08-21 13:08:25 +0200

    init

---
+++ api/src/test/java/shl_nyllet/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package shl_nyllet.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
