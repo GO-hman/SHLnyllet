b0715a7fbe0e08b88024a4e63f0be29382e421f2
Glate Öhman <137804141+GO-hman@users.noreply.github.com>
2026-08-23 22:07:38 +0200

    Feature/angular material (#4)
    * installed angular material + some minor styling
    
    * fix

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,16 +1,36 @@
 import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpErrorResponse } from '@angular/common/http';
-import { GuessPlayerViewInput, GuessPlayerViewOutput, ShlControllerService, ShlPlayer } from '../api';
+import {
+  GuessPlayerViewInput,
+  GuessPlayerViewOutput,
+  ShlControllerService,
+  ShlPlayer,
+} from '../api';
 import { firstValueFrom } from 'rxjs';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSlideToggle } from '@angular/material/slide-toggle';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ReactiveFormsModule, CommonModule],
+  imports: [
+    RouterOutlet,
+    ReactiveFormsModule,
+    CommonModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatSlideToggle,
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('client');
@@ -24,12 +44,11 @@ export class App {
   loading = signal<boolean>(false);
   guessCorrect = signal<boolean | null>(null);
 
-
   form = this.fb.group({
     name: ['', Validators.required],
   });
 
-  async fetchPlayer(){
+  async fetchPlayer() {
     this.loading.set(true);
     this.error.set(null);
     try {
@@ -43,30 +62,44 @@ export class App {
     this.loading.set(false);
   }
 
-  async ngOnInit(){
+  private _snackBar = inject(MatSnackBar);
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action);
+  }
+
+  async ngOnInit() {
     await this.fetchPlayer();
   }
 
-  async onSubmit(){
-    if(this.form.invalid){
-      alert("Invalid form");
+  async onSubmit() {
+    this.guessCorrect.set(null);
+    if (this.form.invalid) {
+      alert('Invalid form');
       return;
     }
     const player = this.currPlayer();
-    if(!player?.uuid){
+    if (!player?.uuid) {
       return;
     }
     try {
-      const response = await firstValueFrom(this.shlService.guessPlayer({
-        id: player.uuid,
-        name: this.form.value.name!,
-      }, 'response'));
+      const response = await firstValueFrom(
+        this.shlService.guessPlayer(
+          {
+            id: player.uuid,
+            name: this.form.value.name!,
+          },
+          'response',
+        ),
+      );
 
       if (response.status === 200) {
         this.guessCorrect.set(true);
         this.fetchPlayer();
+        this.openSnackBar('Rätt', 'x');
       } else {
         this.guessCorrect.set(false);
+        this.openSnackBar('Fel', 'x');
       }
     } catch (err) {
       this.error.set(err instanceof HttpErrorResponse ? err.message : 'Failed to submit guess');
